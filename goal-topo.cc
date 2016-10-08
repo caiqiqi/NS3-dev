9e38e5449bf90c90db0e9198d8275f4e4b939855
caiqiqi <564933437@qq.com>
2016-10-09 03:56:50 +0800

    注释掉当前的Flow id, 因为暂时不需要这个信息

--- goal-topo.cc
+++ goal-topo.cc
@@ -161,7 +161,7 @@ CheckThroughput (FlowMonitorHelper* fmhelper, Ptr<FlowMonitor> flowMon, Gnuplot2
     // `10.0.1.2`是 Node#7   的IP
     if ((t.sourceAddress=="10.0.2.2" && t.destinationAddress == "192.168.0.5"))
       {
-          std::cout << "Flow " << i->first  << " (" << t.sourceAddress << " -> " << t.destinationAddress << ")";
+          //std::cout << "Flow " << i->first  << " (" << t.sourceAddress << " -> " << t.destinationAddress << ")";
           std::cout << "Simulation time: " << Simulator::Now ().GetSeconds () << " s\n";
           localThrou = i->second.rxBytes * 8.0 / (i->second.timeLastRxPacket.GetSeconds() - i->second.timeFirstTxPacket.GetSeconds())/1024/1024 ;
           std::cout << "  Throughput: " <<  localThrou << " Mbps\n";
